90f394188a01ff43a297e32d846a656763fcae81
Jason Bock <jason@jasonbock.net>
2018-03-02 21:35:07 -0600

    Added the walker

---
+++ EditorConfigGenerator.Core.Tests/StyleWalkerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+
+namespace EditorConfigGenerator.Core.Tests
+{
+	[TestFixture]
+	public static class StyleWalkerTests
+	{
+		[Test]
+		public static void VisitForCSharpStyleVarForBuiltInTypes()
+		{
+			var walker = new StyleWalker();
+			var compilationUnit = SyntaxFactory.ParseCompilationUnit(
+@"public static class Test
+{
+	public static void VarFoo()
+	{
+		var a = 1;
+	}
+
+	public static void TypeFoo()
+	{
+		int a = 1;
+	}
+}");
+			walker.Visit(compilationUnit);
+
+			var data = walker.CSharpStyleVarForBuiltInTypesStyle.Data;
+
+			Assert.That(data.TotalOccurences, Is.EqualTo(2u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(1u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(1u), nameof(data.FalseOccurences));
+		}
+	}
+}
---
+++ EditorConfigGenerator.Core/StyleWalker.cs
@@ -0,0 +1,25 @@
+﻿using EditorConfigGenerator.Core.Statistics;
+using EditorConfigGenerator.Core.Styles;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EditorConfigGenerator.Core
+{
+	internal sealed class StyleWalker
+		: CSharpSyntaxWalker
+	{
+		internal StyleWalker()
+			: base(SyntaxWalkerDepth.Trivia) { }
+
+		public override void VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
+		{
+			this.CSharpStyleVarForBuiltInTypesStyle =
+				this.CSharpStyleVarForBuiltInTypesStyle.Update(node);
+			base.VisitLocalDeclarationStatement(node);
+		}
+
+		public CSharpStyleVarForBuiltInTypesStyle CSharpStyleVarForBuiltInTypesStyle { get; private set; } = 
+			new CSharpStyleVarForBuiltInTypesStyle(new BooleanData());
+	}
+}
