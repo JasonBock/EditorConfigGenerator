6169e9c51277789eb8a9b4e12cb685188fa309bb
Jason Bock <jason@jasonbock.net>
2018-03-23 23:17:27 -0500

    Added tests for NodeInformation and ModelNodeInformation

---
+++ EditorConfigGenerator.Core.Tests/Styles/ModelNodeInformationTests.cs
@@ -0,0 +1,61 @@
+﻿using EditorConfigGenerator.Core.Styles;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using System;
+
+namespace EditorConfigGenerator.Core.Tests.Styles
+{
+	[TestFixture]
+	public static class ModelNodeInformationTests
+	{
+		private static readonly CompilationUnitSyntax unit = SyntaxFactory.ParseCompilationUnit("public class F { }");
+
+		[Test]
+		public static void Create()
+		{
+			var tree = ModelNodeInformationTests.unit.SyntaxTree;
+			var compilation = CSharpCompilation.Create(Guid.NewGuid().ToString("N"),
+				new[] { tree },
+				new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) });
+			var model = compilation.GetSemanticModel(tree);
+
+			var information = new ModelNodeInformation<CompilationUnitSyntax>(
+				ModelNodeInformationTests.unit, model);
+			Assert.That(information.Node, Is.SameAs(ModelNodeInformationTests.unit), nameof(information.Node));
+			Assert.That(information.Model, Is.SameAs(model), nameof(information.Model));
+		}
+
+		[Test]
+		public static void CreateWithNullNode()
+		{
+			var tree = ModelNodeInformationTests.unit.SyntaxTree;
+			var compilation = CSharpCompilation.Create(Guid.NewGuid().ToString("N"),
+				new[] { tree },
+				new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) });
+			var model = compilation.GetSemanticModel(tree);
+			Assert.That(() => new ModelNodeInformation<IdentifierNameSyntax>(null, model), Throws.TypeOf<ArgumentNullException>());
+		}
+
+		[Test]
+		public static void CreateWithNullModel() =>
+			Assert.That(() => new ModelNodeInformation<CompilationUnitSyntax>(ModelNodeInformationTests.unit, null), Throws.TypeOf<ArgumentNullException>());
+
+		[Test]
+		public static void DeconstructToTuple()
+		{
+			var tree = ModelNodeInformationTests.unit.SyntaxTree;
+			var compilation = CSharpCompilation.Create(Guid.NewGuid().ToString("N"),
+				new[] { tree },
+				new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) });
+			var model = compilation.GetSemanticModel(tree);
+
+			var information = new ModelNodeInformation<CompilationUnitSyntax>(
+				ModelNodeInformationTests.unit, model);
+			var (newNode, newModel) = information;
+			Assert.That(newNode, Is.SameAs(information.Node), nameof(information.Node));
+			Assert.That(newModel, Is.SameAs(information.Model), nameof(information.Model));
+		}
+	}
+}
---
+++ EditorConfigGenerator.Core.Tests/Styles/NodeInformationTests.cs
@@ -0,0 +1,42 @@
+﻿using EditorConfigGenerator.Core.Styles;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using System;
+
+namespace EditorConfigGenerator.Core.Tests.Styles
+{
+	[TestFixture]
+	public static class NodeInformationTests
+	{
+		[Test]
+		public static void Create()
+		{
+			var node = SyntaxFactory.IdentifierName("f");
+			var information = new NodeInformation<IdentifierNameSyntax>(node);
+			Assert.That(information.Node, Is.SameAs(node));
+		}
+
+		[Test]
+		public static void CreateWithNull() => 
+			Assert.That(() => new NodeInformation<IdentifierNameSyntax>(null), Throws.TypeOf<ArgumentNullException>());
+
+		[Test]
+		public static void DoExplicitCast()
+		{
+			var node = SyntaxFactory.IdentifierName("f");
+			NodeInformation<IdentifierNameSyntax> information = node;
+			Assert.That(information.Node, Is.SameAs(node));
+		}
+
+		[Test]
+		public static void DoImplicitCast()
+		{
+			var node = SyntaxFactory.IdentifierName("f");
+			var information = new NodeInformation<IdentifierNameSyntax>(node);
+			var newNode = (IdentifierNameSyntax)information;
+
+			Assert.That(newNode, Is.SameAs(node));
+		}
+	}
+}
