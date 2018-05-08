38f90d7961d32002bfe13cefadcdc07bb77d6f73
Jason Bock <jason@jasonbock.net>
2018-05-08 18:11:55 -0500

    Finished style

--- EditorConfigGenerator.Core.Tests/Styles/CSharpPreferBracesStyleTests.cs
+++ EditorConfigGenerator.Core.Tests/Styles/CSharpPreferBracesStyleTests.cs
@@ -77,6 +77,22 @@ namespace EditorConfigGenerator.Core.Tests.Styles
 		}
 
 		[Test]
+		public static void UpdateWithUnexpectedNode()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo { }", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.ClassDeclaration) as ClassDeclarationSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(0u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(0u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
 		public static void UpdateWithIfStatementMultiline()
 		{
 			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
@@ -218,6 +234,228 @@ namespace EditorConfigGenerator.Core.Tests.Styles
 			var x = 2;
 	}
 }", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.ElseClause) as ElseClauseSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(1u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(0u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(1u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithForStatementMultiline()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar()
+	{
+		for (var i = 0; i < 10; i++)
+		{
+			var x = 2;
+			var y = x;
+		}
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.ForStatement) as ForStatementSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(0u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(0u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithForStatementOneLineWithBlock()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar()
+	{
+		for (var i = 0; i < 10; i++)
+		{
+			var x = 2;
+		}
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.ForStatement) as ForStatementSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(1u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(1u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithForStatementOneLineWithoutBlock()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar()
+	{
+		for (var i = 0; i < 10; i++)
+			var x = 2;
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.ForStatement) as ForStatementSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(1u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(0u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(1u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithForEachStatementMultiline()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar(string[] items)
+	{
+		foreach (var item in items)
+		{
+			var x = 2;
+			var y = x;
+		}
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.ForEachStatement) as ForEachStatementSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(0u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(0u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithForEachStatementOneLineWithBlock()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar(string[] items)
+	{
+		foreach (var item in items)
+		{
+			var x = 2;
+		}
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.ForEachStatement) as ForEachStatementSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(1u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(1u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithForEachStatementOneLineWithoutBlock()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar(string[] items)
+	{
+		foreach (var item in items)
+			var x = 2;
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.ForEachStatement) as ForEachStatementSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(1u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(0u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(1u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithWhileStatementMultiline()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar(string[] items)
+	{
+		while (true)
+		{
+			var x = 2;
+			var y = x;
+		}
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.WhileStatement) as WhileStatementSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(0u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(0u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithWhileStatementOneLineWithBlock()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar(string[] items)
+	{
+		while (true)
+		{
+			var x = 2;
+		}
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.WhileStatement) as WhileStatementSyntax;
+			var newStyle = style.Update(statement);
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(1u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(1u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithWhileStatementOneLineWithoutBlock()
+		{
+			var style = new CSharpPreferBracesStyle(new BooleanData(default, default, default));
+
+			var statement = SyntaxFactory.ParseCompilationUnit(
+@"public class Foo
+{
+	public void Bar(string[] items)
+	{
+		while (true)
+			var x = 2;
+	}
+}", options: Shared.ParseOptions).DescendantNodes().Single(_ => _.Kind() == SyntaxKind.WhileStatement) as WhileStatementSyntax;
 			var newStyle = style.Update(statement);
 
 			var data = newStyle.Data;
