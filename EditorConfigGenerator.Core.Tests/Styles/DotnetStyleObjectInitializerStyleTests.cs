23f6a29cc7ee72625010bd87582bdef41ae16cf5
Jason Bock <jason@jasonbock.net>
2018-05-12 11:27:04 -0500

    two tests in :)

---
+++ EditorConfigGenerator.Core.Tests/Styles/DotnetStyleObjectInitializerStyleTests.cs
@@ -0,0 +1,141 @@
+﻿using EditorConfigGenerator.Core.Statistics;
+using EditorConfigGenerator.Core.Styles;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace EditorConfigGenerator.Core.Tests.Styles
+{
+	[TestFixture]
+	public static class DotnetStyleObjectInitializerStyleTests
+	{
+		[Test]
+		public static void CreateWithCustomSeverity()
+		{
+			const Severity suggestion = Severity.Suggestion;
+			var data = new BooleanData();
+			var style = new DotnetStyleObjectInitializerStyle(data, suggestion);
+			Assert.That(style.Severity, Is.EqualTo(suggestion), nameof(style.Data));
+		}
+
+		[Test]
+		public static void CreateWithNoData()
+		{
+			var data = new BooleanData();
+			var style = new DotnetStyleObjectInitializerStyle(data);
+			Assert.That(style.Data, Is.SameAs(data), nameof(style.Data));
+			Assert.That(style.GetSetting(), Is.EqualTo(string.Empty), nameof(style.GetSetting));
+		}
+
+		[Test]
+		public static void CreateWithMoreFalseData()
+		{
+			var data = new BooleanData(1u, 0u, 1u);
+			var style = new DotnetStyleObjectInitializerStyle(data);
+			Assert.That(style.Data, Is.SameAs(data), nameof(style.Data));
+			Assert.That(style.GetSetting(), Is.EqualTo("dotnet_style_object_initializer = false:error"), nameof(style.GetSetting));
+		}
+
+		[Test]
+		public static void CreateWithMoreTrueData()
+		{
+			var data = new BooleanData(1u, 1u, 0u);
+			var style = new DotnetStyleObjectInitializerStyle(data);
+			Assert.That(style.Data, Is.SameAs(data), nameof(style.Data));
+			Assert.That(style.GetSetting(), Is.EqualTo("dotnet_style_object_initializer = true:error"), nameof(style.GetSetting));
+		}
+
+		[Test]
+		public static void Add()
+		{
+			var style1 = new DotnetStyleObjectInitializerStyle(new BooleanData(1u, 2u, 3u));
+			var style2 = new DotnetStyleObjectInitializerStyle(new BooleanData(10u, 20u, 30u));
+			var style3 = style1.Add(style2);
+
+			var data = style3.Data;
+			Assert.That(data.TotalOccurences, Is.EqualTo(11u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(22u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(33u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void AddWithNull()
+		{
+			var style = new DotnetStyleObjectInitializerStyle(new BooleanData());
+			Assert.That(() => style.Add(null), Throws.TypeOf<ArgumentNullException>());
+		}
+
+		[Test]
+		public static void UpdateWithNull()
+		{
+			var data = new BooleanData(default, default, default);
+			var style = new DotnetStyleObjectInitializerStyle(data);
+
+			Assert.That(() => style.Update(null), Throws.TypeOf<ArgumentNullException>(), nameof(style.Update));
+		}
+
+		private static (ObjectCreationExpressionSyntax, SemanticModel) GetInformation(CompilationUnitSyntax unit)
+		{
+			var invocation = unit.DescendantNodes().OfType<ObjectCreationExpressionSyntax>().First();
+			var tree = unit.SyntaxTree;
+			var compilation = CSharpCompilation.Create(Guid.NewGuid().ToString("N"),
+				new[] { tree },
+				new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) });
+			var model = compilation.GetSemanticModel(tree);
+
+			return (invocation, model);
+		}
+
+		[Test]
+		public static void UpdateWithCreationThatUsesInitializer()
+		{
+			var style = new DotnetStyleObjectInitializerStyle(new BooleanData(default, default, default));
+			var unit = SyntaxFactory.ParseCompilationUnit(
+@"public class Data { public int Value { get; set; } } 
+
+public class Foo 
+{ 
+	public void Bar()
+	{
+		var x = new Data { Value = 22 };
+	}
+}", options: Shared.ParseOptions);
+			var (node, model) = DotnetStyleObjectInitializerStyleTests.GetInformation(unit);
+			var newStyle = style.Update(new ModelNodeInformation<ObjectCreationExpressionSyntax>(node, model));
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(1u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(1u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+
+		[Test]
+		public static void UpdateWithDiagnostics()
+		{
+			var style = new DotnetStyleObjectInitializerStyle(new BooleanData(default, default, default));
+
+			var unit = SyntaxFactory.ParseCompilationUnit(
+@"public class Data { public int Value { get; set; } } 
+
+public class Foo 
+{ 
+	public void Bar()
+	{
+		var x = new Data=> { Value = 22 };
+	}
+}", options: Shared.ParseOptions);
+			var (node, model) = DotnetStyleObjectInitializerStyleTests.GetInformation(unit);
+			var newStyle = style.Update(new ModelNodeInformation<ObjectCreationExpressionSyntax>(node, model));
+
+			var data = newStyle.Data;
+			Assert.That(newStyle, Is.Not.SameAs(style), nameof(newStyle));
+			Assert.That(data.TotalOccurences, Is.EqualTo(0u), nameof(data.TotalOccurences));
+			Assert.That(data.TrueOccurences, Is.EqualTo(0u), nameof(data.TrueOccurences));
+			Assert.That(data.FalseOccurences, Is.EqualTo(0u), nameof(data.FalseOccurences));
+		}
+	}
+}
