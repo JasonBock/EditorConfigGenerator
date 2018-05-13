79e6b0d8ef733bfa61b29dabebd138f88635d8e6
Jason Bock <jason@jasonbock.net>
2018-05-13 18:57:40 -0500

    Finished DotnetStylePredefinedTypeForLocalsParametersMembersStyle

--- EditorConfigGenerator.Core.Tests/Styles/DotnetStylePredefinedTypeForLocalsParametersMembersStyleTests.cs
+++ EditorConfigGenerator.Core.Tests/Styles/DotnetStylePredefinedTypeForLocalsParametersMembersStyleTests.cs
@@ -6,6 +6,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using NUnit.Framework;
 using System;
 using System.Linq;
+using static EditorConfigGenerator.Core.Extensions.EnumExtensions;
 
 namespace EditorConfigGenerator.Core.Tests.Styles
 {
@@ -36,7 +37,8 @@ namespace EditorConfigGenerator.Core.Tests.Styles
 			var data = new BooleanData(1u, 0u, 1u);
 			var style = new DotnetStylePredefinedTypeForLocalsParametersMembersStyle(data);
 			Assert.That(style.Data, Is.SameAs(data), nameof(style.Data));
-			Assert.That(style.GetSetting(), Is.EqualTo("dotnet_style_predefined_type_for_locals_parameters_members = false:error"), nameof(style.GetSetting));
+			Assert.That(style.GetSetting(), Is.EqualTo(
+				$"{DotnetStylePredefinedTypeForLocalsParametersMembersStyle.Setting} = false:{style.Severity.GetDescription()}"), nameof(style.GetSetting));
 		}
 
 		[Test]
@@ -45,7 +47,8 @@ namespace EditorConfigGenerator.Core.Tests.Styles
 			var data = new BooleanData(1u, 1u, 0u);
 			var style = new DotnetStylePredefinedTypeForLocalsParametersMembersStyle(data);
 			Assert.That(style.Data, Is.SameAs(data), nameof(style.Data));
-			Assert.That(style.GetSetting(), Is.EqualTo("dotnet_style_predefined_type_for_locals_parameters_members = true:error"), nameof(style.GetSetting));
+			Assert.That(style.GetSetting(), Is.EqualTo(
+				$"{DotnetStylePredefinedTypeForLocalsParametersMembersStyle.Setting} = true:{style.Severity.GetDescription()}"), nameof(style.GetSetting));
 		}
 
 		[Test]
--- EditorConfigGenerator.Core/Styles/DotnetStylePredefinedTypeForLocalsParametersMembersStyle.cs
+++ EditorConfigGenerator.Core/Styles/DotnetStylePredefinedTypeForLocalsParametersMembersStyle.cs
@@ -12,6 +12,8 @@ namespace EditorConfigGenerator.Core.Styles
 	public sealed class DotnetStylePredefinedTypeForLocalsParametersMembersStyle
 		: ModelSeverityNodeStyle<BooleanData, SyntaxNode, ModelNodeInformation<SyntaxNode>, DotnetStylePredefinedTypeForLocalsParametersMembersStyle>
 	{
+		public const string Setting = "dotnet_style_predefined_type_for_locals_parameters_members";
+
 		public DotnetStylePredefinedTypeForLocalsParametersMembersStyle(BooleanData data, Severity severity = Severity.Error)
 			: base(data, severity) { }
 
@@ -26,7 +28,7 @@ namespace EditorConfigGenerator.Core.Styles
 			if (this.Data.TotalOccurences > 0)
 			{
 				var value = this.Data.TrueOccurences >= this.Data.FalseOccurences ? "true" : "false";
-				return $"dotnet_style_predefined_type_for_locals_parameters_members = {value}:{this.Severity.GetDescription()}";
+				return $"{DotnetStylePredefinedTypeForLocalsParametersMembersStyle.Setting} = {value}:{this.Severity.GetDescription()}";
 			}
 			else
 			{
