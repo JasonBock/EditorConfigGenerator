c50a2e9b1a8ec9e8c54bc4777317420ee4a0ace0
Jason Bock <jason@jasonbock.net>
2018-05-22 19:52:17 -0500

    Got tests for NodeStyle and TokenStyle

---
+++ EditorConfigGenerator.Core.Tests/Styles/NodeStyleTests.cs
@@ -0,0 +1,40 @@
+﻿using EditorConfigGenerator.Core.Statistics;
+using EditorConfigGenerator.Core.Styles;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+using System;
+
+namespace EditorConfigGenerator.Core.Tests.Styles
+{
+	[TestFixture]
+	public static class NodeStyleTests
+	{
+		[Test]
+		public static void Create()
+		{
+			var data = new BooleanData();
+			var style = new TestStyle(data);
+			Assert.That(style.Data, Is.SameAs(data));
+		}
+
+		[Test]
+		public static void CreateWhenDataIsNull() =>
+			Assert.That(() => new TestStyle(null), Throws.TypeOf<ArgumentNullException>());
+
+		private sealed class TestStyle
+			: NodeStyle<BooleanData, SyntaxNode, NodeInformation<SyntaxNode>, TestStyle>
+		{
+			public TestStyle(BooleanData data) 
+				: base(data) { }
+
+			public override TestStyle Add(TestStyle style) => 
+				throw new NotImplementedException();
+
+			public override string GetSetting() => 
+				throw new NotImplementedException();
+
+			public override TestStyle Update(NodeInformation<SyntaxNode> information) => 
+				throw new NotImplementedException();
+		}
+	}
+}
---
+++ EditorConfigGenerator.Core.Tests/Styles/TokenStyleTests.cs
@@ -0,0 +1,40 @@
+﻿using EditorConfigGenerator.Core.Statistics;
+using EditorConfigGenerator.Core.Styles;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+using System;
+
+namespace EditorConfigGenerator.Core.Tests.Styles
+{
+	[TestFixture]
+	public static class TokenStyleTests
+	{
+		[Test]
+		public static void Create()
+		{
+			var data = new BooleanData();
+			var style = new TestStyle(data);
+			Assert.That(style.Data, Is.SameAs(data));
+		}
+
+		[Test]
+		public static void CreateWhenDataIsNull() =>
+			Assert.That(() => new TestStyle(null), Throws.TypeOf<ArgumentNullException>());
+
+		private sealed class TestStyle
+			: TokenStyle<BooleanData, TokenInformation, TestStyle>
+		{
+			public TestStyle(BooleanData data) 
+				: base(data) { }
+
+			public override TestStyle Add(TestStyle style) => 
+				throw new NotImplementedException();
+
+			public override string GetSetting() => 
+				throw new NotImplementedException();
+
+			public override TestStyle Update(TokenInformation information) => 
+				throw new NotImplementedException();
+		}
+	}
+}
